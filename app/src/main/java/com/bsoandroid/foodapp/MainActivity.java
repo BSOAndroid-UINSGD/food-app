14465f3d5b3f9781c98556abb85a65851b0a3ba4
AkhdanFirdaus <akhdan.musyaffa.firdaus@gmail.com>
2021-11-01 20:51:23 +0700

    Fungsi login

--- app/src/main/java/com/bsoandroid/foodapp/MainActivity.java
+++ app/src/main/java/com/bsoandroid/foodapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.bsoandroid.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Ambil nilai inputan
+        TextView username = findViewById(R.id.username_text);
+        TextView password = findViewById(R.id.password_text);
+
+        //Cek apakah username dan password benar
+        if (username.getText() == "test" && password.getText() == "123456") {
+            //Jika iya pindah activity
+            //Menggunakan Explicit Intent
+            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+            startActivity(intent);
+            this.finish();
+        }  else {
+            //Jika tidak tampilkan pesan error
+            Toast toast = Toast.makeText(getApplicationContext(), "Username/Password salah", Toast.LENGTH_SHORT);
+        }
     }
 }
