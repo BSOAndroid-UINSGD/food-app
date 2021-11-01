b6d079c9d69122ad4530bbe5e4e8cd7363e0fee4
AkhdanFirdaus <akhdan.musyaffa.firdaus@gmail.com>
2021-11-01 21:04:22 +0700

    Action toolbar dan logout

--- app/src/main/java/com/bsoandroid/foodapp/HomeActivity.java
+++ app/src/main/java/com/bsoandroid/foodapp/HomeActivity.java
@@ -1,8 +1,13 @@
 package com.bsoandroid.foodapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.app_menu_item, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout_item:
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                this.finish();
+            default:
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
